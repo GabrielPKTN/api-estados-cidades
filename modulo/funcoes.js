13645c3cff2114c1dcbbb962e6fdff389b05c061
GABRIEL LACERDA CORREIA <25132902@CFP127ALUNO.LOCAL>
2025-09-17 11:21:02 -0300

    Terminando de desenvolver as funções da aplicação, iniciando tratativas de erros das funções

--- modulo/funcoes.js
+++ modulo/funcoes.js
@@ -17,22 +17,28 @@ const MESSAGE_ERROR = {
 // Retorna todos os estados
 const getAllEstados = () => {
     
+    // Variável de base para cabeçalho da API
     let message = {
         status: true,
         status_code: 200,
         development: "Gabriel Lacerda Correia",
         uf:[
-            // Estados
+            
         ]
     }
 
-    dados.listaDeEstados.estados.forEach(function (item) {
+    // Loop
+    dados.listaDeEstados.estados.forEach((item) => {
         message.uf.push(item.sigla)
     })
 
     message.quantidade = message.uf.length
 
+    // Adicionando elemento novo no JSON
     result = message.uf.length > 0 ? message : MESSAGE_ERROR
+
+    // Método que remove atributos de um objeto
+    delete message.status
 
     return result
 }
@@ -40,34 +46,191 @@ const getAllEstados = () => {
 // Retorna um estado pela sigla
 const getEstadoBySigla = (sigla) => {
 
+    sigla = sigla.toUpperCase()
 
+    let message = {
+        status: true,
+        status_code: 200,
+        development: "Gabriel Lacerda Correia",
+        result: {
 
+        }
+    }
+
+    dados.listaDeEstados.estados.forEach((item) => {
+    
+        if (item.sigla == sigla) {
+    
+            message.result.uf = item.sigla
+            message.result.descricao = item.nome
+            message.result.capital = item.capital
+            message.result.regiao = item.regiao
+    
+        }
+    
+    })
+
+    result = dados.listaDeEstados.estados
+    .find(estados => estados.sigla === sigla) ? message : MESSAGE_ERROR
+
+    return result
 }
 
 // Retorna informações da capital referente a sigla do estado
 const getCapitalBySigla = (sigla) => {
 
+    sigla = sigla.toUpperCase()
+
+    let message = {
+        status: true,
+        status_code: 200,
+        development: "Gabriel Lacerda Correia",
+        result: {
+
+        }
+    }
+
+    dados.listaDeEstados.estados.forEach((item) => {
+    
+        if (item.sigla == sigla) {
+    
+            message.result.uf = item.sigla
+            message.result.descricao = item.nome
+            message.result.capital = item.capital
+    
+        }
+    
+    })
+
+    result = dados.listaDeEstados.estados
+    .find(estados => estados.sigla === sigla) ? message : MESSAGE_ERROR
+
+    return result
 }
 
 // Retorna informações dos estados referente a região
 const getEstadosByRegiao = (regiao) => {
 
+
+    let message = {
+        status: true,
+        status_code: 200,
+        development: "Gabriel Lacerda Correia",
+        estados: [
+
+        ]
+    }
+
+    dados.listaDeEstados.estados.forEach( (item) => {
+       
+        if(item.regiao == regiao) {
+           
+            let sigla = item.sigla
+            let capital = item.capital
+
+            message.estados.push({ sigla, capital })
+        }
+
+    })
+
+    result = dados.listaDeEstados.estados
+    .find(estados => estados.regiao == regiao) ? message : MESSAGE_ERROR
+
+    return message
 }
 
 // Retorna informações dos estados que foram a capital do país, e qual é
 // atualmente
 const getVerifyCapitaisDoPais = () => {
 
+    let message = {
+        status: true,
+        status_code: 200,
+        development: "Gabriel Lacerda Correia",
+        capitais: [
+
+        ]
+    }
+
+    dados.listaDeEstados.estados.forEach( (item) => {
+
+        if (item.capital_pais) {
+
+            let capital_atual = item.capital_pais.capital
+            let uf = item.sigla
+            let descricao = item.nome
+            let capital = item.capital
+            let regiao = item.regiao
+            let capital_pais_ano_inicio = item.capital_pais.ano_inicio
+            let capital_pais_ano_fim = item.capital_pais.ano_fim
+
+
+            message.capitais.push(
+                {
+                    capital_atual,
+                    uf,
+                    descricao,
+                    capital,
+                    regiao,
+                    capital_pais_ano_inicio,
+                    capital_pais_ano_fim
+
+                })
+        }
+
+    })
+
+    console.log(message)
 }
 
 // Retorna uma lista de cidades do estado pela sigla
 const getCidadesBySigla = (sigla) => {
+    
+    let message = {
+        status: true,
+        status_code: 200,
+        development: "Gabriel Lacerda Correia",
+        
+    }
 
+    dados.listaDeEstados.estados.forEach((item) => {
+
+        if (sigla == item.sigla) {
+
+            let cidades = []
+            let qtd_cidades = 0
+
+            item.cidades.forEach((cidade) => {
+
+                cidades.push(cidade.nome)
+                qtd_cidades += 1
+
+            })
+
+            message.uf = item.sigla
+            message.descricao = item.nome
+            message.quantidade_cidades = qtd_cidades
+            message.cidades = cidades
+           
+        }
+
+    })
+    
+    return message
 }
 
 // getAllEstados()
-getEstadoBySigla('SP')
+// console.log(getEstadoBySigla('po'))
+// console.log(getCapitalBySigla('po'))
+console.log(getEstadosByRegiao('Norteste'))
+// getVerifyCapitaisDoPais()
+// getCidadesBySigla('SP')
 
 module.exports = {
-    getAllEstados
+    getAllEstados,
+    getEstadoBySigla,
+    getCapitalBySigla,
+    getEstadosByRegiao,
+    getVerifyCapitaisDoPais,
+    getCidadesBySigla
 }
