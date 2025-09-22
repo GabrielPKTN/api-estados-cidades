9b575e3e6ae09f4c6554193645b22c89f95e19eb
GABRIEL LACERDA CORREIA <25132902@CFP127ALUNO.LOCAL>
2025-09-22 08:29:53 -0300

    Adicionando endpoints de pesquisa de estados, e capitais, pela sigla de estado

--- app.js
+++ app.js
@@ -15,24 +15,58 @@
 const express    = require('express')        // Responsável pela API
 const cors       = require('cors')           // Responsável pelas permissões da API (APP)
 const bodyParser = require('body-parser')    // Responsável por gerenciar a chegada dos dados da API com o front
+const dados = require('./modulo/funcoes.js')
 
 // Retorna a porta do servidor atual ou colocamos uma porta local.
 const PORT = process.PORT || 8000
 
 // Criando uma instancia de uma classe do express
-const  APP = express()
+const  app = express()
 
 // Configuração de permissões
-APP.use((request, response, next) => {
+app.use((request, response, next) => {
     response.header('Access-Control-Allow-Origin', '*')    // Servidor de origem
     response.header('Access-Control-Allow-Methods', 'GET') // Verbos permitidos
     
     // Carrega as configurações no CORS da API
-    APP.use(cors())
+    app.use(cors())
     next() // Próximo, carregar os próximos EndPoints 
 })
 
+// Request  -> chegada de dados na API
+// Response -> retorno de dados na API
+
 // EndPoints
-APP.get('/v1/estados/', (request, response) => {
+app.get('/v1/estados/', (request, response) => {
     
+    // Pesquisa na função de estados
+    let estados = dados.getAllEstados()
+    
+    // Retorna o status code e JSON
+    response.status(estados.status_code).json(estados)
+    
+})
+
+app.get('/v1/estados/:uf', (request, response) => {
+    
+    let estado = dados.getEstadoBySigla(request.params.uf)
+
+    response.status(estado.status_code).json(estado)
+
+})
+
+app.get('/v1/capital/:uf', (request, response) => {
+
+    let capital = dados.getCapitalBySigla(request.params.uf)
+
+    response.status(capital.status_code).json(capital)
+    
+})
+
+
+
+
+// Start na API
+app.listen(PORT, () => {
+    console.log('Está vivo...!!!')
 })
